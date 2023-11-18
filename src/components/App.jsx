40e17524f538d805dae0650f4028979fac67a902
Jhon Esparragoza <jhonantonyesp@gmail.com>
2023-11-18 13:49:13 +0100

    added mapping

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,29 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
 //Tip: falta colocar un bucle para que se carguen las Cards con la info de cada contacto de contacs.js
+
+function CreateCard(contact){
+  return (
+    <Card 
+      key = {contact.id}
+      name = {contact.name}
+      imgURL = {contact.imgURL}
+      phone = {contact.phone}
+      email = {contact.email}
+    />
+  )
+}
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-        <Card
-            name = {contacts[0].name}
-            imgURL ={contacts[0].imgURL}
-            phone = {contacts[0].phone}
-            email = {contacts[0].email}
+        <Avatar 
+          imgURL = "https://scontent.ftrn5-1.fna.fbcdn.net/v/t39.30808-6/402555457_10232253928001620_5663571794557634277_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=7Mo1PdNeHOIAX9sY1c9&_nc_ht=scontent.ftrn5-1.fna&oh=00_AfBKeqztWq0Oaz7AHuh5WFfx646LlquhVzduTuB7ioiiIw&oe=655A715F"
         />
-        <Card
-            name = {contacts[1].name}
-            imgURL ={contacts[1].imgURL}
-            phone = {contacts[1].phone}
-            email = {contacts[1].email}
-        />
-        <Card
-            name = {contacts[2].name}
-            imgURL ={contacts[2].imgURL}
-            phone = {contacts[2].phone}
-            email = {contacts[2].email}
-        />
+        {contacts.map(CreateCard)}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,21 @@
 import React from "react";
+import Avatar from "./Avatar";
+import Details from "./Details";
 
 function Card(props){
 return(
 <div className="card">
     <div className="top">
       <h2 className="name">{props.name}</h2>
-      <img
-        src={props.imgURL}
-        alt="avatar_img"
-        className="circle-img"
+      <Avatar 
+        imgURL = {props.imgURL}
       />
     </div>
     <div className="bottom">
-        <div className="info">
-            <p>{props.phone}</p>
-            <p>{props.email}</p>
-        </div>
+        <Details 
+          phone = {props.phone}
+          email = {props.email}
+        />
     </div>
   </div>
 )
